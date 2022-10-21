5a8701900562f5fd1a1cfd681b23b985c08a8fc9
RushabhS21 <110185943+RushabhS21@users.noreply.github.com>
2022-10-21 22:42:36 +0530

    Update stackusingarray.c

--- stackusingarray.c
+++ stackusingarray.c
@@ -1,1 +1,65 @@
+#include<stdio.h>
+#define N 20
+typedef struct stack
+{
+	int a[N];
+	int top;
+}stack;
 
+void push(stack *s,int x)
+{
+	if(s->top==N-1)
+		printf("\nStack Overflow...");
+	else
+	{
+		s->top=s->top+1;
+		s->a[s->top]=x;
+	}
+}
+
+int isempty(stack *s)
+{
+	if(s->top==-1)
+		return 1;
+	else
+		return 0;
+}
+
+int pop(stack *s)
+{
+	int x;
+	if(isempty(s))
+	{
+		return -1;
+	}
+	else
+	{
+		x=s->a[s->top];
+		s->top=s->top-1;
+		return x;
+	}
+}
+
+int peek(stack *s)
+{
+	if(isempty(s))
+		return -1;
+	else
+		return s->a[s->top];
+}
+
+void display(stack *s)
+{
+	int i;
+	if(isempty(s))
+	{
+		printf("\nStack is empty...");
+	}
+	else
+	{
+		for(i=s->top;i>=0;i--)
+		{
+			printf("\n%d",s->a[i]);
+		}
+	}
+}
